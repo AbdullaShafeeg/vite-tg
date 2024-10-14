ab9b7ade771794a27380b0b62751fc672aee51c2
AbdullaShafeeg <abuquanta@gmail.com>
2024-10-14 15:27:02 +0400

    update UI

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,28 +1,27 @@
-import { Section, Cell, Image, List } from '@telegram-apps/telegram-ui';
+import { Section, Textarea, Cell,  Button, List } from '@telegram-apps/telegram-ui';
 import type { FC } from 'react';
 
 import { Link } from '@/components/Link/Link.tsx';
 import { Page } from '@/components/Page.tsx';
 
-import tonSvg from './ton.svg';
+
 
 export const IndexPage: FC = () => {
   return (
     <Page back={false}>
       <List>
+        {/* Header Section */}
         <Section
-          header="Features"
-          footer="You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects"
-        >
-          <Link to="/ton-connect">
-            <Cell
-              before={<Image src={tonSvg} style={{ backgroundColor: '#007AFF' }}/>}
-              subtitle="Connect your TON wallet"
-            >
-              TON Connect
-            </Cell>
-          </Link>
+        style={{
+          background: '#13a412',
+          height: '53px',
+        }}>
+        <Cell>
+          <Button>A</Button>
+        </Cell>
         </Section>
+
+        {/* Content Section */}
         <Section
           header="Application Launch Data"
           footer="These pages help developer to learn more about current launch information"
@@ -36,6 +35,19 @@ export const IndexPage: FC = () => {
           <Link to="/theme-params">
             <Cell subtitle="Telegram application palette information">Theme Parameters</Cell>
           </Link>
+        </Section>
+
+        {/* ChatInput Section */}
+        <Section>
+          <Cell>
+            <Button>B</Button>
+            <Textarea
+            placeholder='Message'
+            style={{
+              background: '#323232',
+            }}>
+            </Textarea>
+          </Cell>
         </Section>
       </List>
     </Page>
